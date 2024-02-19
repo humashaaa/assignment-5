fdd9ddfbde0696ba3f45359d3b1573654c1edc93
humashaaa <afridakhan2121@gmail.com>
2024-02-19 23:31:08 +0600

    another commit

--- script.js
+++ script.js
@@ -2,20 +2,10 @@
 
 // 2.  and grand total price jog hobe
 // bonus-
-// coupon btn disable coupon code use korle apply kora possible, apply click korle coupon input hide hobe discount price koto paise display hobe, bhul coupon code dile alert dite hobe and 4 ta seat book korle unable hobe [disable nei]
 // 4 tar beshi kew select korte parbe na, korle alert dekhate hobe
 // new 15 hole grand total price e 15 % discount hobe
 // couple 20 dile 20% discount
 
-
-
-
-
-// updated task-
-
-
-
-// 2. bonus- 4 ta seat select, coupon apply korle discount, grand total price e, 
 const allBtn = document.getElementsByClassName('add-btn');
 
 let count = 0;
@@ -72,95 +62,42 @@ for(const btn of allBtn){
 
     div.classList.add("flex" , "text-base" , "inter-medium" , "text-gray-400" , "gap-40");
 
-    const applyBtn = document.getElementById('apply-btn');
-applyBtn.addEventListener('click' , function(){
+    counter = counter + 1;
+    console.log(counter);
+
+    if(counter <= 4){
+        const discountAmount = document.getElementById('discount-amount');
+            sum = sum *0.15;
+
+            discountAmount.innerText = sum;
+            return;
+        
+            console.log( )
+        }
+        else{
+            e.target.setAttribute('disable', true);
+            alert('You can book 4 seats at a time!');
+      }
+      applyBtn();
+    })
+
+    
+
+     function applyBtn(){
 
     // get the value from input
     const couponElement = document.getElementById('input-field');
     const couponCode = couponElement.value;
     
-    counter = counter + 1;
-    
-
-    if(counter = 4){
         if(couponCode === "NEW15" ){
             const inputDiscount = document.getElementById('input');
             inputDiscount.classList.add("hidden");
             const discountPrice = document.getElementById('discount-price');
             discountPrice.classList.remove('hidden');
-            console.log('bravo')
-            
-            const discountAmount = document.getElementById('discount-amount');
-            sum = sum *0.15;
-
-            discountAmount.innerText = price;
-            
-
-            console.log( )
-
-        }
+           }
         else{
-            alert('Invalid coupon');
-        }
-
-
-    }
-    else{
-        alert('You have to book 4 seats to get the discount!')
-    }
-})
-
-    })
-}
-
-// coupon button
-// const applyBtn = document.getElementById('apply-btn');
-// applyBtn.addEventListener('click' , function(){
-
-//     // get the value from input
-//     const couponElement = document.getElementById('input-field');
-//     const couponCode = couponElement.value;
-
+          alert('Invalid coupon')
+         }
+  }
     
-
-//     if(getseat > 2200){
-//         if(couponCode === "NEW15" ){
-//             const inputDiscount = document.getElementById('input');
-//             inputDiscount.classList.add("hidden");
-//             const discountPrice = document.getElementById('discount-price');
-//             discountPrice.classList.remove('hidden');
-//             console.log('bravo')
-            
-//             const discountAmount = document.getElementById('discount-amount');
-//             price = price *0.15;
-
-//             discountAmount.innerText = price;
-            
-
-//             console.log( )
-
-//         }
-//         else{
-//             alert('Invalid coupon');
-//         }
-
-
-//     }
-//     else{
-//         alert('You have to book 4 seats to get the discount!')
-//     }
-// })
-
-
-    // function setInnerText (id, value){
-        // document.getElementById(id).innerText = value;
-    // }
-
-
-
-
-
-
-
-
-
+}
